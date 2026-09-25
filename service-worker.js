7cc99dda0d1af938de3446c255f763dba31b02af
nicholaschilala-claudepages <nicholas.chilala@gmail.com>
2026-09-25 10:07:29 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // neue Version bekommen.
 // ============================================================================
 
-const CACHE_NAME = 'dadi-plattform-v7';
+const CACHE_NAME = 'dadi-plattform-v8';
 const APP_SHELL = [
   './',
   'index.html',
